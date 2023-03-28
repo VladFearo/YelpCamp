ed299bc098c4354eab7e6e3c6aa2e1f90c09eee1
Vlad <69718459+VladFearo@users.noreply.github.com>
2023-03-28 18:08:53 +0300

    Add files via upload
    bug fixes and added flash and routers

--- seeds/index.js
+++ seeds/index.js
@@ -1,36 +1,40 @@
 const mongoose = require('mongoose');
-const Campground = require('../models/campgrounds');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open",()=>{
+db.once("open", () => {
     console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random()* array.length)];
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
-        const random1000 = Math.floor(Math.random()*1000);
-        const price = Math.floor(Math.random()* 20) + 10
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus alias, fugiat excepturi iure impedit vero. Dolorum sequi hic incidunt facere quasi eius sed quod, est fugiat laborum aut, iure ut.',
-            price: price
-
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
         })
         await camp.save();
     }
 }
 
-seedDB().then(()=>{
+seedDB().then(() => {
     mongoose.connection.close();
-});
+})
