657c6ccebf7c898df9cff8d7403de34bcc6fc1f5
Vlad Shevtsov <vladsh123@gmail.com>
2024-09-17 15:31:36 +0300

    ADD ejs-mate support

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
 
 const Campground = require('./models/campground.js');
 
@@ -21,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 
 app.get('/', (req, res) => {
